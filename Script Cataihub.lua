86bafcbb70a275e12d1519fd2daf8203cf607ce2
CataiHub <165255694+Cataihub@users.noreply.github.com>
2024-05-08 10:46:47 +0700

    Update Script Cataihub.lua

--- Script Cataihub.lua
+++ Script Cataihub.lua
@@ -2,7 +2,7 @@ local player = game.Players.LocalPlayer
 
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
 
-local Window = OrionLib:MakeWindow({Name = "Key system", HidePremium = false, SaveConfig = true, IntroEnabled = false})
+local Window = OrionLib:MakeWindow({Name = "Cataihub | Key system", HidePremium = false, SaveConfig = true, IntroEnabled = false})
 
 OrionLib:MakeNotification({
 	Name = "Logged In!",
